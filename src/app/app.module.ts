ca81dc141d498c8452411a2478eec6c2c0201594
Edison Devadoss <edisonj1996@gmail.com>
2017-10-26 13:29:33 +0530

    firebase Analytics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import { FirebaseAnalytics } from '@ionic-native/firebase-analytics';
+
 @NgModule({
   declarations: [
     MyApp,
@@ -23,6 +25,7 @@ import { HomePage } from '../pages/home/home';
   ],
   providers: [
     StatusBar,
+    FirebaseAnalytics,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,40 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { FirebaseAnalytics } from '@ionic-native/firebase-analytics';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private firebaseAnalytics: FirebaseAnalytics) {
 
+  }
+  firebaseAnalytic(){
+    this.firebaseAnalytics.logEvent('Firebase_analytics', {page: "dashboard"})
+    .then((res: any)=>{
+        console.log(res);
+    })
+    .catch((err)=>{
+      console.log(err);
+    })
+
+    this.firebaseAnalytics.setCurrentScreen('Firebase Analytics')
+    .then((res)=>{
+      console.log("Sucessful", res)
+    })
+    .catch((err)=>{
+      console.log('Error', err);
+    })
+
+    this.firebaseAnalytics.setUserId("12345")
+    .then((res)=>{
+      console.log(res);
+    })
+    .catch((err)=>{
+      console.log(err);
+    })
   }
 
 }
